36297f23931d80eef7522248cc2b7024e6c5976b
Thomas Spitzer <t.spitzer@astade.de>
2023-09-11 14:57:24 +0200

    made the create edge

--- Model/Package_Astade/Package_Astade_Graphic_library/Package_Nodes/classes/class_glActivity/operations/operation_SaveDrawIo/code.cpp
+++ Model/Package_Astade/Package_Astade_Graphic_library/Package_Nodes/classes/class_glActivity/operations/operation_SaveDrawIo/code.cpp
@@ -4,7 +4,7 @@ cr << "        <mxCell id=\"Node:"
    << id
    << "\" value=\""
    << myLabel.utf8_str()
-   << "\" style=\"shape=step;perimeter=stepPerimeter;whiteSpace=wrap;html=1;fixedSize=1;fontSize=10;spacingLeft=0;spacingRight=0;spacing=22;fillColor=#fff2cc;\" vertex=\"1\" parent=\"1\">\n";
+   << "\" style=\"shape=step;perimeter=stepPerimeter;whiteSpace=wrap;html=1;fixedSize=1;fontSize=10;spacingLeft=0;spacingRight=0;spacing=22;strokeColor=#E1D5E7;fillColor=#fff2cc;\" vertex=\"1\" parent=\"1\">\n";
 cr << "          <mxGeometry x=\""
    << absGetDrawPosition().xCoord() - my_XRadius
    << "\" y=\""
--- Model/Package_Astade/Package_Astade_Graphic_library/Package_Nodes/classes/class_glArtefact/operations/operation_SaveDrawIo/code.cpp
+++ Model/Package_Astade/Package_Astade_Graphic_library/Package_Nodes/classes/class_glArtefact/operations/operation_SaveDrawIo/code.cpp
@@ -4,7 +4,7 @@ cr << "        <mxCell id=\"Node:"
    << id
    << "\" value=\""
    << myLabel.utf8_str()
-   << "\" style=\"shape=note;perimeter=stepPerimeter;whiteSpace=wrap;html=1;fontSize=10;spacingLeft=0;spacingRight=0;spacing=22;fillColor=#0050ef;size=20;\" vertex=\"1\" parent=\"1\">\n";
+   << "\" style=\"shape=note;perimeter=rectanglePerimeter;whiteSpace=wrap;html=1;fontSize=10;spacingLeft=0;spacingRight=0;spacing=22;strokeColor=#E1D5E7;fillColor=#0050ef;size=20;\" vertex=\"1\" parent=\"1\">\n";
 cr << "          <mxGeometry x=\""
    << absGetDrawPosition().xCoord() - my_XRadius
    << "\" y=\""
